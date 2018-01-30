c9de5ab28a90d1bbf31522f8c6c3f6e4905d3cee
zbloom4 <zbloom@mail.csuchico.edu>
2018-01-29 21:23:34 -0800

    - add home page

--- FemForce/resources/views/template.blade.php
+++ FemForce/resources/views/template.blade.php
@@ -8,7 +8,7 @@
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="apple-touch-fullscreen" content="yes">
 
-        <link rel="icon" type="image/x-icon" href="{{ secure_asset('img/favicon.ico') }}">
+        <link rel="icon" type="image/x-icon" href="{{ asset('img/favicon.ico') }}">
 
 
         <link href='//fonts.googleapis.com/css?family=RobotoDraft:300,400,400italic,500,700' rel='stylesheet' type='text/css'>
@@ -16,33 +16,33 @@
         <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 
         <!-- Favicon -->
-        <link rel="shortcut icon" href="{{secure_asset('theme/jobboard-free-lite/assets/img/favicon.png')}}">
+        <link rel="shortcut icon" href="{{asset('theme/jobboard-free-lite/assets/img/favicon.png')}}">
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="{{secure_asset('theme/jobboard-free-lite/assets/css/bootstrap.min.css')}}" type="text/css">
-        <link rel="stylesheet" href="{{secure_asset('theme/jobboard-free-lite/assets/css/jasny-bootstrap.min.css')}}" type="text/css">
-        <link rel="stylesheet" href="{{secure_asset('theme/jobboard-free-lite/assets/css/bootstrap-select.min.css')}}" type="text/css">
+        <link rel="stylesheet" href="{{asset('theme/jobboard-free-lite/assets/css/bootstrap.min.css')}}" type="text/css">
+        <link rel="stylesheet" href="{{asset('theme/jobboard-free-lite/assets/css/jasny-bootstrap.min.css')}}" type="text/css">
+        <link rel="stylesheet" href="{{asset('theme/jobboard-free-lite/assets/css/bootstrap-select.min.css')}}" type="text/css">
         <!-- Material CSS -->
-        <link rel="stylesheet" href="{{secure_asset('theme/jobboard-free-lite/assets/css/material-kit.css')}}" type="text/css">
+        <link rel="stylesheet" href="{{asset('theme/jobboard-free-lite/assets/css/material-kit.css')}}" type="text/css">
         <!-- Font Awesome CSS -->
-        <link rel="stylesheet" href="{{secure_asset('theme/jobboard-free-lite/assets/fonts/font-awesome.min.css')}}" type="text/css">
-        <link rel="stylesheet" href="{{secure_asset('theme/jobboard-free-lite/assets/fonts/themify-icons.css')}}">
+        <link rel="stylesheet" href="{{asset('theme/jobboard-free-lite/assets/fonts/font-awesome.min.css')}}" type="text/css">
+        <link rel="stylesheet" href="{{asset('theme/jobboard-free-lite/assets/fonts/themify-icons.css')}}">
 
         <!-- Animate CSS -->
-        <link rel="stylesheet" href="{{secure_asset('theme/jobboard-free-lite/assets/extras/animate.css')}}" type="text/css">
+        <link rel="stylesheet" href="{{asset('theme/jobboard-free-lite/assets/extras/animate.css')}}" type="text/css">
         <!-- Owl Carousel -->
-        <link rel="stylesheet" href="{{secure_asset('theme/jobboard-free-lite/assets/extras/owl.carousel.css')}}" type="text/css">
-        <link rel="stylesheet" href="{{secure_asset('theme/jobboard-free-lite/assets/extras/owl.theme.css')}}" type="text/css">
+        <link rel="stylesheet" href="{{asset('theme/jobboard-free-lite/assets/extras/owl.carousel.css')}}" type="text/css">
+        <link rel="stylesheet" href="{{asset('theme/jobboard-free-lite/assets/extras/owl.theme.css')}}" type="text/css">
         <!-- Rev Slider CSS -->
-        <link rel="stylesheet" href="{{secure_asset('theme/jobboard-free-lite/assets/extras/settings.css')}}" type="text/css">
+        <link rel="stylesheet" href="{{asset('theme/jobboard-free-lite/assets/extras/settings.css')}}" type="text/css">
         <!-- Slicknav js -->
-        <link rel="stylesheet" href="{{secure_asset('theme/jobboard-free-lite/assets/css/slicknav.css')}}" type="text/css">
+        <link rel="stylesheet" href="{{asset('theme/jobboard-free-lite/assets/css/slicknav.css')}}" type="text/css">
         <!-- Main Styles -->
-        <link rel="stylesheet" href="{{secure_asset('theme/jobboard-free-lite/assets/css/main.css')}}" type="text/css">
+        <link rel="stylesheet" href="{{asset('theme/jobboard-free-lite/assets/css/main.css')}}" type="text/css">
         <!-- Responsive CSS Styles -->
-        <link rel="stylesheet" href="{{secure_asset('theme/jobboard-free-lite/assets/css/responsive.css')}}" type="text/css">
+        <link rel="stylesheet" href="{{asset('theme/jobboard-free-lite/assets/css/responsive.css')}}" type="text/css">
 
         <!-- Color CSS Styles  -->
-        <link rel="stylesheet" type="text/css" href="{{secure_asset('theme/jobboard-free-lite/assets/css/colors/red.css')}}" media="screen" />
+        <link rel="stylesheet" type="text/css" href="{{asset('theme/jobboard-free-lite/assets/css/colors/red.css')}}" media="screen" />
 
     </head>
 
@@ -131,7 +131,7 @@
                             <div class="row">
                                 <div class="col-md-3 col-sm-6 col-xs-12">
                                     <div class="widget">
-                                        <h3 class="block-title"><img src="{{secure_asset('FemmForce_Logo-2.png')}}" class="img-responsive" alt="Footer Logo"></h3>
+                                        <h3 class="block-title"><img src="{{asset('FemmForce_Logo-2.png')}}" class="img-responsive" alt="Footer Logo"></h3>
                                         <div class="textwidget">
                                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque lobortis tincidunt est, et euismod purus suscipit quis. Etiam euismod ornare elementum. Sed ex est, consectetur eget facilisis sed.</p>
                                         </div>
@@ -196,22 +196,22 @@
                 </a>
 
                 <!-- Main JS  -->
-                <script type="text/javascript" src="{{secure_asset('theme/jobboard-free-lite/assets/js/jquery-min.js')}}"></script>
-                <script type="text/javascript" src="{{secure_asset('theme/jobboard-free-lite/assets/js/bootstrap.min.js')}}"></script>
-                <script type="text/javascript" src="{{secure_asset('theme/jobboard-free-lite/assets/js/material.min.js')}}"></script>
-                <script type="text/javascript" src="{{secure_asset('theme/jobboard-free-lite/assets/js/material-kit.js')}}"></script>
-                <script type="text/javascript" src="{{secure_asset('theme/jobboard-free-lite/assets/js/jquery.parallax.js')}}"></script>
-                <script type="text/javascript" src="{{secure_asset('theme/jobboard-free-lite/assets/js/owl.carousel.min.js')}}"></script>
-                <script type="text/javascript" src="{{secure_asset('theme/jobboard-free-lite/assets/js/jquery.slicknav.js')}}"></script>
-                <script type="text/javascript" src="{{secure_asset('theme/jobboard-free-lite/assets/js/main.js')}}"></script>
-                <script type="text/javascript" src="{{secure_asset('theme/jobboard-free-lite/assets/js/jquery.counterup.min.js')}}"></script>
-                <script type="text/javascript" src="{{secure_asset('theme/jobboard-free-lite/assets/js/waypoints.min.js')}}"></script>
-                <script type="text/javascript" src="{{secure_asset('theme/jobboard-free-lite/assets/js/jasny-bootstrap.min.js')}}"></script>
-                <script type="text/javascript" src="{{secure_asset('theme/jobboard-free-lite/assets/js/bootstrap-select.min.js')}}"></script>
-                <script type="text/javascript" src="{{secure_asset('theme/jobboard-free-lite/assets/js/form-validator.min.js')}}"></script>
-                <script type="text/javascript" src="{{secure_asset('theme/jobboard-free-lite/assets/js/contact-form-script.js')}}"></script>
-                <script type="text/javascript" src="{{secure_asset('theme/jobboard-free-lite/assets/js/jquery.themepunch.revolution.min.js')}}"></script>
-                <script type="text/javascript" src="{{secure_asset('theme/jobboard-free-lite/assets/js/jquery.themepunch.tools.min.js')}}"></script>
+                <script type="text/javascript" src="{{asset('theme/jobboard-free-lite/assets/js/jquery-min.js')}}"></script>
+                <script type="text/javascript" src="{{asset('theme/jobboard-free-lite/assets/js/bootstrap.min.js')}}"></script>
+                <script type="text/javascript" src="{{asset('theme/jobboard-free-lite/assets/js/material.min.js')}}"></script>
+                <script type="text/javascript" src="{{asset('theme/jobboard-free-lite/assets/js/material-kit.js')}}"></script>
+                <script type="text/javascript" src="{{asset('theme/jobboard-free-lite/assets/js/jquery.parallax.js')}}"></script>
+                <script type="text/javascript" src="{{asset('theme/jobboard-free-lite/assets/js/owl.carousel.min.js')}}"></script>
+                <script type="text/javascript" src="{{asset('theme/jobboard-free-lite/assets/js/jquery.slicknav.js')}}"></script>
+                <script type="text/javascript" src="{{asset('theme/jobboard-free-lite/assets/js/main.js')}}"></script>
+                <script type="text/javascript" src="{{asset('theme/jobboard-free-lite/assets/js/jquery.counterup.min.js')}}"></script>
+                <script type="text/javascript" src="{{asset('theme/jobboard-free-lite/assets/js/waypoints.min.js')}}"></script>
+                <script type="text/javascript" src="{{asset('theme/jobboard-free-lite/assets/js/jasny-bootstrap.min.js')}}"></script>
+                <script type="text/javascript" src="{{asset('theme/jobboard-free-lite/assets/js/bootstrap-select.min.js')}}"></script>
+                <script type="text/javascript" src="{{asset('theme/jobboard-free-lite/assets/js/form-validator.min.js')}}"></script>
+                <script type="text/javascript" src="{{asset('theme/jobboard-free-lite/assets/js/contact-form-script.js')}}"></script>
+                <script type="text/javascript" src="{{asset('theme/jobboard-free-lite/assets/js/jquery.themepunch.revolution.min.js')}}"></script>
+                <script type="text/javascript" src="{{asset('theme/jobboard-free-lite/assets/js/jquery.themepunch.tools.min.js')}}"></script>
             </section>   
         </div>
 
