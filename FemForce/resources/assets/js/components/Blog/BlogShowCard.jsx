9d791be5e14970440c0ed2c35f2394aaf0bb1016
zbloom4 <zach@pocketpoints.com>
2018-04-17 21:26:41 -0700

    - Single blog view

--- FemForce/resources/assets/js/components/Blog/BlogShowCard.jsx
+++ FemForce/resources/assets/js/components/Blog/BlogShowCard.jsx
@@ -27,10 +27,12 @@ var BlogViewCard = React.createClass({
 
     getHeader: function() {
         return (
-            <div>
-                <div style={{position: "relative"}}>
+            <div className="row">
+                <div className="col-md-6">
                     <img src={secureURL + "/" + this.state.blog.get('image').get('path')} height={350} width={540}/>
-                    <div className="blog-title" style={{position: "absolute", left: "160px", top: "250px", width: "200px", height: "200px"}}>
+                </div>
+                <div className="col-md-6">
+                    <div className="blog-title" style={{position: "absolute", left: "160px", top: "80px", width: "200px", height: "200px"}}>
                         <hr style={{height: "4px", border: "none", color: "#333", backgroundColor: "#333", width: "25%", strokeWidth: "1px", marginLeft: "20px", marginRight: "20px"}}/>
                         <h4 style={{textTransform: "uppercase", fontWeight: "bold", marginLeft: "20px", marginRight: "20px"}}>{this.state.blog.get('title')}</h4>
                         <hr style={{height: "4px", border: "none", color: "#333", backgroundColor: "#333", width: "25%", strokeWidth: "1px", marginLeft: "20px", marginRight: "20px"}}/>
@@ -51,7 +53,7 @@ var BlogViewCard = React.createClass({
     },
 
     getHtmlContent: function() {
-        var htmlInput = '<div>' + this.state.blog.get('html_content') + '</div>';
+        var htmlInput = '<div class="row col-md-12" style="margin-top: 20px; margin-bottom: 20px">' + this.state.blog.get('html_content') + '</div>';
         var htmlToReactParser = new HtmlToReact.Parser(React);
         return htmlToReactParser.parse(htmlInput);
     },
